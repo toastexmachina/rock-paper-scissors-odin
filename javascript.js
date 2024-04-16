4e215ad6ce8d96377b99ff9f9ead8a5cb3b962fb
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-04-16 11:12:09 -0500

    write pseudocode for the function getComputerChoice

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// function to randomly return rock, paper, or scissors
+
+// declare a function called getComputerChoice
+// randomly return 1, 2, or 3
+// conditonal if 1, then rock
+// conditional if 2, then paper
+// conditional if 3, then scissors
+// variable to store the outcome
+// return the result
+
