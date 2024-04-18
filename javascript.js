7cb28d4b4eb5378bcd868cce62c1ada36df27706
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-04-18 13:17:44 -0500

    Incorporate playRound function into for-loop and add player/computer wins into playGame function.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
+/*
 // process to return a computer selection
 let randomNumber = Math.floor(Math.random()*3 + 1);
+*/
 
 function getComputerChoice(x) {
     return (x == 1) ? 'Rock':
@@ -7,43 +9,57 @@ function getComputerChoice(x) {
     (x == 3) ? 'Scissors' :
     "";
 }
-
+/*
 // computer and player selection
 let computerSelection = getComputerChoice(randomNumber).toLowerCase();
 let playerSelection = prompt("What is your choice?");
 playerSelection.toLowerCase();
-
+*/
 
 // function that plays a single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissor' && computerSelection == 'paper')) {
-        return true;
+        pScore = pScore + 1;
     } else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'rock')) {
-        return false;
+        cScore = cScore + 1;
     } else {
-        return null;
+        pScore = pScore;
+        cScore = cScore;
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-
 // function to play a 5 round game that keeps score and reports a winner or loser at the end
+    // create variables that update the score for each player
+       // pScore = pScore + 1;
+       // cScore = sScore + 1;
 // track wins and losses
 // ignore ties
 // alert score 
+     //   alert(pScore, cScore);
 // end after five games
 //
 
 function playGame() {
-
-}
-
+    pScore = 0;
+    cScore = 0;
 // loop conditions p and c that tracks the number of wins.
-for (let i=0; i<6; i++) {
+    for (let i=0; i<6; i++) {
 
+        let randomNumber = Math.floor(Math.random()*3 + 1);
+        
+        let computerSelection = getComputerChoice(randomNumber).toLowerCase();
+        
+        let playerSelection = prompt("What is your choice?");
+        playerSelection.toLowerCase();
+        
+        playRound(computerSelection, playerSelection);
+        alert("the player score is " + pScore + "." + " and the computer score is " + cScore + ".")
+        }
 }
+console.log(playGame());
+
 
