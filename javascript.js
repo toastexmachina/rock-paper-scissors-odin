6323ff07b52b630d1740897b304a9b249ff6eee3
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-06-27 16:01:51 -0400

    Add score tracker that updates with each round

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
-
 // Function that converts a random integer input of 1-3 into a computerSelection of "rock", "paper", or "scissors"
 function getComputerChoice(x) {
-    return (x == 1) ? 'Rock':
-    (x == 2) ? 'Paper':
-    (x == 3) ? 'Scissors' :
+    return (x == 1) ? 'rock':
+    (x == 2) ? 'paper':
+    (x == 3) ? 'scissors' :
     "";
 }
 
@@ -11,15 +10,12 @@ function getComputerChoice(x) {
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock') ||
-        (playerSelection == 'scissor' && computerSelection == 'paper')) {
-        pScore = pScore + 1;
+        (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        pScore++
     } else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'rock')) {
-        cScore = cScore + 1;
-    } else {
-        pScore = pScore;
-        cScore = cScore;
+        cScore++
     }
 }
 
@@ -32,9 +28,6 @@ let cScore = 0;
 let playerScore = document.querySelector('#playerScore');
 let computerScore = document.querySelector('#computerScore');
 
-playerScore.textContent = `Player: ${pScore}`
-computerScore.textContent = `Computer: ${cScore}`
-
 function playGame(playerSelection) {
     let randomNumber = Math.floor(Math.random()*3 + 1);
     let computerSelection = getComputerChoice(randomNumber).toLowerCase();
@@ -42,6 +35,9 @@ function playGame(playerSelection) {
     playRound(playerSelection, computerSelection);
 
     results.textContent = `You played ${playerSelection}. The Computer played ${computerSelection}`
+
+    playerScore.textContent = `Player: ${pScore}`
+    computerScore.textContent = `Computer: ${cScore}`
 }
 
 // button event listener
@@ -56,24 +52,5 @@ btnPaper.addEventListener("click", function(){
     playGame("paper")
 });
 btnScissor.addEventListener("click", function(){
-    playGame("scissor")
+    playGame("scissors")
 });
-
-// function to play a game of 5 rounds and track wins and losses 
-/*
-// loop conditions p and c that tracks the number of wins.
-    for (let i=0; i<5; i++) {
-        let randomNumber = Math.floor(Math.random()*3 + 1);
-        let computerSelection = getComputerChoice(randomNumber).toLowerCase();
-        
-        let playerSelection = prompt("Rock... Paper... Scissors... Shoot!");
-        playerSelection.toLowerCase();
-        
-        playRound(computerSelection, playerSelection);
-        alert("You played " + playerSelection + ". The computer played " + computerSelection + ".")
-        alert("the player score is " + pScore + "." + "The computer score is " + cScore + ".")
-        }
-        */
-
-
-
